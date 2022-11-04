c672be10c888dc9b4a0fa29f8e587c228ced6e8b
jonpbak <jonpbak@vt.edu>
2022-11-04 10:51:18 -0400

    Polishing solver demo

--- demo_solve.m
+++ demo_solve.m
@@ -11,11 +11,10 @@ r = 2;
 % Number of quadrature points
 N = 500;
 
+% Set up evenly-spaced quadrature points on circular contour
 theta = linspace(0,2*pi,N+1);
 theta = theta(1:end-1);
 unit_circle = exp(1i*theta);
-
-% Evenly-spaced quadrature points on circular contour
 z = c + r*unit_circle;
 % Quadrature weights
 w = 2i*pi*r/N*unit_circle;
@@ -25,5 +24,13 @@ k = 2;
 % Number of probing directions
 p = 7;
 
+% Use NLEVP solver
 e = basic_solver(T,z,w,p,k)
+
+% Plot the contour and eigenvalues
+clf
+hold on
+plot(real(z),imag(z),'k-')
+plot(real(e),imag(e),'ko')
+axis equal
 
