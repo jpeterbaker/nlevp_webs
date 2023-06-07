f98a3686add1a6c7aadd764f0efe73b70798e9a6
jonpbak <jonpbak@vt.edu>
2023-06-07 12:22:19 -0400

    Graph drawing routine, force-balanced regular webs

--- regweb.m
+++ regweb.m
@@ -1,8 +1,9 @@
 function [T,TV,gamma,nodes,edges] = regweb(spokes,rings,d)
 %function [T,TV,gamma,nodes,edges] = regweb(spokes,rings,d)
 %
-% Represent modal vibrations of a network of elastic strings as a nonlinear eigenvalue problem
-% The network is a "spider web" with regularly spaced spokes and rings
+% Represent modal vibrations of a network of elastic strings as a nonlinear eigenvalue problem.
+% The network is a "spider web" with regularly spaced spokes and rings.
+% The tension in each string is chosen to balance the forces at each free node.
 %
 % For this problem
 %    Nodes        nv = spokes*(rings+1)+1
@@ -37,5 +38,32 @@ end
 
 [nodes,edges] = regweb_graph(spokes,rings,d);
 
-[T,TV,gamma] = general_web(nodes,edges);
+rho = 1;
+k   = 1;
+
+%--------------------------------------%
+% Find force-balancing stretch factors %
+%--------------------------------------%
+
+% For mathematical convenience, each ring strand has the same tension,
+% center spokes (connected to the hub) have tension 1 (stretch factor s=2),
+% and anchored spokes (on the perimeter) have tension 2 (s=3).
+
+% Inner angle between spoke and rings
+theta = (0.5-1/spokes)*pi;
+% Tension in each ring (scalar)
+r_tension = sec(theta)/(2*rings);
+% s_tension(i) is spoke tension between rings (i-1) and i
+s_tension = linspace(1,2,rings+1);
+% Now replicate since there are (spokes) spoke-strings between consecutive rings
+s_stretch = reshape(repmat(1+s_tension/k,spokes,1),[],1);
+
+% Stretch factor s for all strings in the order provided by regweb_graph
+% tension = k*(s-1)
+s = [...
+    s_stretch; ... % Spokes
+    repmat(1+r_tension/k,rings*spokes,1)... % Rings
+];
+
+[T,TV,gamma] = general_web(nodes,edges,rho,k,s);
 
